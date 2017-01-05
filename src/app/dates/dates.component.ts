2f53d2d40ff7e24616151543b1e3a8af96b5d47a
Martin Nieratschker <mniera@vie.bgt.ag>
2017-01-05 13:40:36 +0100

    Fixed selection of dates

--- src/app/dates/dates.component.ts
+++ src/app/dates/dates.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+class GameDate {
+  name: string;
+  date: Date;
+  isSelected: boolean;
+}
+
 @Component({
   selector: 'app-dates',
   templateUrl: './dates.component.html',
@@ -7,20 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatesComponent implements OnInit {
 
-  dates: string[] = [];
+  dates: GameDate[] = [];
+  currentDate: Date;
 
   constructor() { }
 
   ngOnInit() {
-    let date = new Date();
+    let currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
 
+    let dates = this.createDates(currentDate);
+    console.log(dates);
+    this.dates = dates.map(date => {
+      return <GameDate>{
+        date: date,
+        name: date.toLocaleDateString(),
+        isSelected: false
+      };
+    });
+
+    this.dates[0].isSelected = true;
+  }
+
+  createDates(currentDate: Date): Date[] {
+    let date = currentDate;
+    console.log(currentDate.getDay());
+
+    let dates: Date[] = [];
     while (date.getDay() !== 4) {
       date.setDate(date.getDate() + 1);
     }
 
     for (let i = 0; i < 5; i++) {
-      this.dates.push(date.toLocaleDateString());
-      date.setDate(date.getDate() + 7);
+      let x = new Date();
+      x.setHours(0, 0, 0, 0);
+      x.setDate(date.getDate() + 7 * i);
+      dates.push(x);
     }
+
+    return dates;
   }
 }
