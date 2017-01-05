df91f61169629f9f92b2ce9a0a6d58c71244fbcc
Martin Nieratschker <mniera@vie.bgt.ag>
2017-01-05 12:39:37 +0100

    Added date stamps

--- src/app/dates/dates.component.ts
+++ src/app/dates/dates.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatesComponent implements OnInit {
 
+  dates: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
-  }
+    let date = new Date();
 
+    while (date.getDay() !== 4) {
+      date.setDate(date.getDate() + 1);
+    }
+
+    for (let i = 0; i < 5; i++) {
+      this.dates.push(date.toLocaleDateString());
+      date.setDate(date.getDate() + 7);
+    }
+  }
 }
